ef4fe320695ffe63a2cf0f14c166a6ccff0ef8f3
Francisco Ugalde <javierugalde@gmail.com>
2019-05-20 17:06:43 +0200

    added missing migrations

---
+++ Application/src/Api/Shared/Infrastructure/Migrations/Version20190519223051.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\Shared\Infrastructure\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190519223051 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE group_teams ADD CONSTRAINT FK_C6EE093FFE54D947 FOREIGN KEY (group_id) REFERENCES tournament_group (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE group_teams DROP FOREIGN KEY FK_C6EE093FFE54D947');
+    }
+}
