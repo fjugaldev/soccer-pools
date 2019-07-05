6a612a542dd937d5af5238230307a46692a42aeb
Francisco Ugalde <fjugal.dev@franciscougalde.com>
2019-07-05 23:43:59 +0200

    [SP-3] - Refactoring Hexagonal Architecture and DDD

---
+++ Application/src/Domain/Team/ValueObject/Team.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Domain\Team\ValueObject;
+
+use App\Domain\Shared\ValueObject\BaseEntity;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class Team extends BaseEntity
+{
+    const SERVER_PATH_TO_IMAGE_FOLDER = './uploads/flags';
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $fifaCode;
+
+    /**
+     * @var string
+     */
+    private $iso2;
+
+    /**
+     * @var string
+     */
+    private $flag;
+
+    /**
+     * Unmapped property to handle file uploads
+     */
+    private $file;
+
+    /**
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    public function getFile(): ?UploadedFile
+    {
+        return $this->file;
+    }
+
+    /**
+     * Manages the copying of the file to the relevant place on the server
+     */
+    public function upload(): void
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // we use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and target filename as params
+        $this->getFile()->move(
+            self::SERVER_PATH_TO_IMAGE_FOLDER,
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->flag = $this->getFile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->setFile(null);
+    }
+
+    /**
+     * Lifecycle callback to upload the file to the server.
+     */
+    public function lifecycleFileUpload(): void
+    {
+        $this->upload();
+    }
+
+    /**
+     * Updates the hash value to force the preUpdate and postUpdate events to fire.
+     */
+    public function refreshUpdated(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Team
+     */
+    public function setName(string $name): Team
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getFifaCode(): ?string
+    {
+        return $this->fifaCode;
+    }
+
+    /**
+     * @param string $fifaCode
+     * @return Team
+     */
+    public function setFifaCode(string $fifaCode): Team
+    {
+        $this->fifaCode = $fifaCode;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getIso2(): ?string
+    {
+        return $this->iso2;
+    }
+
+    /**
+     * @param string $iso2
+     * @return Team
+     */
+    public function setIso2(string $iso2): Team
+    {
+        $this->iso2 = $iso2;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    /**
+     * @param string $flag
+     * @return Team
+     */
+    public function setFlag(string $flag): Team
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebPath(): string
+    {
+        return self::SERVER_PATH_TO_IMAGE_FOLDER.'/'.$this->getFlag();
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+}
---
+++ Application/src/UI/Http/BackOffice/Admin/TournamentAdmin.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\UI\Http\BackOffice\Admin;
+
+use App\Domain\Tournament\ValueObject\Tournament;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Sonata\Form\Type\DateTimePickerType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TournamentAdmin extends AbstractAdmin
+{
+    protected $baseRouteName = 'admin_tournament';
+    protected $baseRoutePattern = 'tournament';
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        // get the current Image instance
+        $image = $this->getSubject();
+
+        // use $fileFieldOptions so we can add other options to the field
+        $fileFieldOptions = ['required' => false];
+        if ($image && ($webPath = $image->getWebPath())) {
+            // get the container so the full path to the image can be set
+            $container = $this->getConfigurationPool()->getContainer();
+            $basePath = $container->get('request_stack')->getCurrentRequest()->getBasePath();
+            $basePath = 'http://localhost:8008';
+            $fullPath = $basePath.'/'.$webPath;
+            $preview = $image->getFile() !== null
+                ? '<img src="'.$fullPath.'" class="admin-preview" style="border: 1px solid #000; width: 10%;" />'
+                : '';
+
+            // add a 'help' option containing the preview's img tag
+            $fileFieldOptions = [
+                'help' => $preview,
+                'required' => true,
+                'label' => 'Logo',
+            ];
+        }
+
+        $formMapper
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('federation', ModelAutocompleteType::class, [
+                'property' => 'name',
+                'multiple' => false,
+                'to_string_callback' => function($entity, $property) {
+                    return $entity->getName();
+                },
+            ], [
+                'placeholder' => 'No federation selected',
+            ])
+            ->add('fromDate', DateTimePickerType::class)
+            ->add('toDate', DateTimePickerType::class)
+            ->add('file', FileType::class, $fileFieldOptions)
+        ;
+    }
+
+    public function prePersist($team)
+    {
+        $this->manageFileUpload($team);
+    }
+
+    public function preUpdate($team)
+    {
+        $this->manageFileUpload($team);
+    }
+
+    private function manageFileUpload(Tournament $tournament)
+    {
+        if ($tournament->getFile()) {
+            $tournament->refreshUpdated();
+        }
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name', null, [
+                'show_filter' => true
+            ])
+            ->add('fromDate', 'doctrine_orm_datetime', array('field_type' => DateTimePickerType::class,))
+            ->add('toDate', 'doctrine_orm_datetime', array('field_type' => DateTimePickerType::class,))
+            ->add('federation')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id');
+        $listMapper->addIdentifier('name');
+        $listMapper->add('federation');
+        $listMapper->add('fromDate');
+        $listMapper->add('toDate');
+        $listMapper->add('logo', null, [
+            'template' => "Admin/Team/list_thumb.html.twig"
+        ]);
+    }
+}
